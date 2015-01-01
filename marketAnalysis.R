451953bdab8ae8a2b98e7aaed15a6d6dac71e07c
0xack13 <0xack13@gmail.com>
2015-01-01 12:07:53 +0300

    Add nutshell package

--- marketAnalysis.R
+++ marketAnalysis.R
@@ -1,7 +1,10 @@
 #install.packages("arules")
 library(arules)
 audioscrobbler <- read.transactions(
-  file="D:\\profiledata_06-May-2005\\user_artist_data.txt",
+  file="C:\\Downloads\\profiledata_06-May-2005\\profiledata_06-May-2005\\user_artist_data.txt",
   format="single",
   sep=",",
   cols=c(1,2))
+
+#install.packages("nutshell")
+library(nutshell)
