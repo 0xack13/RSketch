55354aa8f0a2aa1cfebaa64f9c8145bb776f5576
Saleh Albatati <0xack13@gmail.com>
2015-01-06 01:52:49 +0300

    Apriori method

--- Apriori.R
+++ Apriori.R
@@ -3,3 +3,5 @@ install.packages("arulesViz")
 library("arulesViz")
 #inside arulesViz package
 data("Groceries")
+summary(Groceries)
+rules <- apriori(Groceries, parameter=list(support=0.001, confidence=0.5))
