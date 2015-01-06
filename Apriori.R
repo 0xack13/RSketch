1f5cb2ec759aa6f05741b2e1a6a4801132fca11d
Saleh Albatati <0xack13@gmail.com>
2015-01-07 00:33:28 +0300

    Plot rules

--- Apriori.R
+++ Apriori.R
@@ -8,5 +8,6 @@ rules <- apriori(Groceries, parameter=list(support=0.001, confidence=0.5))
 # Number of the association rules
 rules
 # Inspect 4 of'em only
-inspect(head(sort(rules, by ="lift"),4))
+inspect(head(sort(rules, by ="lift"),4pl))
 plot(x, method = NULL, measure = "support", shading = "lift", interactive = FALSE, data = NULL, control = NULL) 
+plot(rules)
